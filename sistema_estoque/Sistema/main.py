4b2cbe284cd3b29af305d4a9c7cc9c8d90ab25b2
Winicius Bertoncello <winiciusberton@gmail.com>
2026-03-19 21:58:22 -0300

    Alterações no data.py, pequenas alterações no produto.py devido a mudanças feitas pelo gabe e mudanças na lista encadeada devido a função inserir implementada no produto.py. Primeira versão do main.py feita, falta importar e coisas no main.py, como por exemplo o menu (? não sei onde botar ou se crio outro .py

---
+++ sistema_estoque/Sistema/main.py
@@ -0,0 +1,7 @@
+from cliente import Cliente
+from produto import Produto
+from venda import Venda
+from lista_encadeada import Nolista
+from fila import fila
+from pilha import PilhaOperacoes
+from sistema_estoque.Dados.data import criar_arquivos
